c4f2c56b9768988e1ebee9608d866fe0d6597486
LIU Yu-Jun <laus_hisaki_liu@LIUs-MacBook-Pro-2.local>
2026-05-14 21:41:43 +0800

    20260514

--- I/execute.sh
+++ I/execute.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 python3 --version
-
+g++ --version
 # chmod +x ./execute.sh (change the permission of the file)
 
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
     source .venv/bin/activate
+    pip list
+    pip install -r ./I/pylibs.txt
 else
     echo "Virtual environment already exists. Activating it."
     source .venv/bin/activate
 fi
-
-pip list
-# pip install -r pylibs.txt
 
 deactivate
 # Run the Python script
